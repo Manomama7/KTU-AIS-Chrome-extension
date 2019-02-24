83d418955e306f97ebbfce9c1c908ac37eee3777
Martynas Stankevičius <martynoemailas@gmail.com>
2019-02-24 15:52:43 +0200

    Change nested Promise chains to async/await

--- src/Utils/KtuApi.js
+++ src/Utils/KtuApi.js
@@ -4,6 +4,7 @@ import { getModules, getMarkHtmlByModule, isLoginPage } from './MarkScraper';
 
 const ALARM_NAME = 'newMarkAlarm';
 const MARK_CHECKING_PERIOD = 1.5; // In minutes
+const NOTIFIED_NOT_LOGGED_IN = 'KTU_AIS_NOT_LOGGED_IN_NOTIFIED';
 
 function Notify(message) {
   chrome.notifications.create({
@@ -14,6 +15,7 @@ function Notify(message) {
   });
 }
 
+const getFromStorage = key => new Promise(resolve => chrome.storage.local.get(key, resolve));
 
 async function checkNewMarks() {
   const modules = await getModules();
@@ -25,8 +27,6 @@ async function checkNewMarks() {
   // These two lines act as a sort of hash for easy equality comparison
   marks.sort();
   marks = JSON.stringify(marks);
-
-  const getFromStorage = what => new Promise(resolve => chrome.storage.local.get(what, resolve));
 
   const oldMarks = await getFromStorage(['marks']);
 
@@ -55,32 +55,42 @@ export async function isLoggedIn() {
 }
 
 const getAlarm = alarmName => new Promise(resolve => chrome.alarms.get(alarmName, resolve));
-
-export const startupAction = () => {
-  getAlarm(ALARM_NAME)
-    .then((alarm) => {
-      if (alarm === undefined) {
-        startTimer();
-      }
-    });
+const setInStorage = (key, value) => {
+  chrome.storage.local.set({
+    [key]: value,
+  });
 };
 
-export const timedAction = (alarm) => {
+export const startupAction = async () => {
+  setInStorage(NOTIFIED_NOT_LOGGED_IN, false);
+
+  const alarm = await getAlarm(ALARM_NAME);
+
+  if (alarm === undefined) {
+    startTimer();
+  }
+};
+
+export const timedAction = async (alarm) => {
   if (alarm.name === ALARM_NAME) {
-    isLoggedIn()
-      .then((loggedIn) => {
-        if (!loggedIn) {
-          Notify('You need to login to KTU AIS to continue checking for marks');
-        } else {
-          checkNewMarks()
-            .then((newMarks) => {
-              if (newMarks) {
-                Notify('There are new marks!');
-              } else {
-                console.log('There are no new marks');
-              }
-            });
-        }
-      });
+    const loggedIn = await isLoggedIn();
+
+    if (!loggedIn) {
+      const { [NOTIFIED_NOT_LOGGED_IN]: alreadyNotified } = await getFromStorage(NOTIFIED_NOT_LOGGED_IN);
+
+      if (!alreadyNotified) {
+        Notify('You need to login to KTU AIS to continue checking for marks');
+        setInStorage(NOTIFIED_NOT_LOGGED_IN, true);
+      }
+    } else {
+      setInStorage(NOTIFIED_NOT_LOGGED_IN, false);
+
+      const newMarks = await checkNewMarks();
+      if (newMarks) {
+        Notify('There are new marks!');
+      } else {
+        console.log('There are no new marks');
+      }
+    }
   }
 };
